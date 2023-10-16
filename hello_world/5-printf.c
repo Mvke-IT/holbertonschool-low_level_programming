46748106fb2d85d9cb0fd14494039e230fa51432
Mvke-IT <michael.duarte09@gmail.com>
2023-10-16 10:34:45 -0400

    p

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+/**
+ * main - printf formats and reads the data
+ *
+ * Return: will make the value of void = 0
+ */
+int main(void)
+{
+	printf("\" with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
