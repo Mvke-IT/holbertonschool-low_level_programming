f7da30d6d511b8005c7d2f43178571028f389bd9
Mvke-IT <michael.duarte09@gmail.com>
2023-10-16 10:17:03 -0400

    p

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * Main puts will print the text inside of the quatation 
+ * Main puts will print the text inside of the quatation
  *
  * Return will make the value of void = 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
