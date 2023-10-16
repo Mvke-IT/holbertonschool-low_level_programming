f942f564edfa9a1192f51eb3ccc2e724f89fc842
Mvke-IT <michael.duarte09@gmail.com>
2023-10-16 10:01:54 -0400

    p

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+/**
+ * Main Starts the source code
+ *
+ * Return Ends the source code
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
