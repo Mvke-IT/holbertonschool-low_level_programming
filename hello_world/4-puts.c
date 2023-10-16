a30b8319276b0aff8bf18d74b9c425a7267e56b6
Mvke-IT <michael.duarte09@gmail.com>
2023-10-16 10:20:45 -0400

    p

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * Main puts will print the text inside of the quatation
+ * main - puts will print the text inside of the quatation
  *
- * Return will make the value of void = 0
+ * Return: will make the value of void = 0
  */
 int main(void)
 {
