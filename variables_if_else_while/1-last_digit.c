fd0556121754d5f777a28dcbccea21c6c1bae3b0
Mvke-IT <michael.duarte09@gmail.com>
2023-10-17 17:28:20 -0400

    p

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,7 +3,8 @@
 #include <time.h>
 
 /* Describe the main function. */
-int main(void) {
+int main(void)
+{
 	int n, lstd;
 
 	srand(time(0));
@@ -14,9 +15,11 @@ int main(void) {
 	printf("Last digit of %d is %d\n", n, lstd);
 
 	/* Check if the last digit of n is greater than 5. */
-	if (lstd > 5) {
+	if (lstd > 5)
+	{
 		printf(" and is greater than 5\n");
-	} else if (lstd == 0) {
+	} else if (lstd == 0)
+	{
 		printf(" and is 0\n");
 	} else if (lstd < 6 && lstd != 0) {
 		printf(" and is less than 6 and not 0\n");
