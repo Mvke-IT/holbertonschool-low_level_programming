64a0a01477499f1f5fa9c686b8e8c16625774bba
Mvke-IT <michael.duarte09@gmail.com>
2023-10-18 09:17:29 -0400

    p

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Function prints a string of lowercase letters, followed by a new line character.
+* Return: The program prints out the alphabet in lowercase, followed by a new line.
+*/
+int main(void)
+{
+char c;
+while (c <= 'z')
+{
+putchar(c);
+putchar('\n');
+return (0);
+}
