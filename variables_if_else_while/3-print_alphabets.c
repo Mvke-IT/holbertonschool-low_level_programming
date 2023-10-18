33227dfafded934523fcb668984efc81b3058058
Mvke-IT <michael.duarte09@gmail.com>
2023-10-18 11:13:10 -0400

    p

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print alphabet
+ * Return: to 0
+ */
+int main(void)
+{
+	char c = 'a', d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	while (d <= 'Z')
+	{
+		putchar(d)
+		d++;
+	}
+	putchar ('\n');
+	return (0);
+}
