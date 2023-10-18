bd04f9f75bc14d8b9739c18ebc3ffb36ab20ea82
Mvke-IT <michael.duarte09@gmail.com>
2023-10-18 09:26:20 -0400

    p

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -3,15 +3,16 @@
 #include <stdio.h>
 
 /**
-* main - Function prints a string of lowercase letters, followed by a new line character.
-* Return: The program prints out the alphabet in lowercase, followed by a new line.
-*/
+ * main - Function prints a string of lowercase letters, followed by a new line character.
+ * Return: The program prints out the alphabet in lowercase, followed by a new line.
+ */
 int main(void)
 {
-char c;
-while (c <= 'z')
-{
-putchar(c);
-putchar('\n');
-return (0);
+	char c;
+	while (c <= 'z')
+	{
+		putchar(c);
+		putchar('\n');
+		return (0);
+	}
 }
