dda9e53b8fc61fe5aef979d0499a5d3772c0a722
Mvke-IT <michael.duarte09@gmail.com>
2023-10-18 14:02:05 -0400

    p

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Display all single-digit numbers in base 10, starting from 0.
+ * Return: to 0
+ */
+int main(void)
+{
+	int i = 0;
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
