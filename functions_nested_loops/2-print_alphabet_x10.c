649c2a1a5e874f168bc13eb3cc97a232e5a68c53
Mvke-IT <michael.duarte09@gmail.com>
2023-10-19 15:26:03 -0400

    p

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * print_alphabet_x10 - print alphabet times 10.
+ */
+
+void print_alphabet_x10(void)
+{
+	int num = 0;
+	char letter;
+
+	while (num <= 10)
+	{
+		for (letter = 'a' ; letter <= 'z' ; letter ++)
+			_putchar(letter);
+
+		_putchar('\n');
+		num++;
+	}
+}
