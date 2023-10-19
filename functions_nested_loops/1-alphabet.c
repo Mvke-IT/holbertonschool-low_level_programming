2e4f434abb72f64e9bc9ef563dacc8af88b30dca
Mvke-IT <michael.duarte09@gmail.com>
2023-10-19 13:11:35 -0400

    p

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - print the alphabet in lower case
+ */
+
+void print_alphabet(void)
+
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+
+		_putchar(alphabet);
+		alphabet++;
+	}
+	_putchar ('\n');
+}
