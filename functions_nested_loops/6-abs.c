e58154da21357b5886f4e72a4582faaf349ae714
Mvke-IT <michael.duarte09@gmail.com>
2023-10-20 14:40:18 -0400

    p

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolue value of an integer
+ *@n: the int checked
+ * Return: the absolute value of int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
