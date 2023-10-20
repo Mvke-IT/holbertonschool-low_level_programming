272d88b2cb332e8a449b0c870c5098f58c18a2ee
Mvke-IT <michael.duarte09@gmail.com>
2023-10-20 19:03:39 -0400

    push

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - addition a + b
+ * @a: somme ajouter a b
+ * @b: somme ajouter a a
+ * Return: somme de a + b
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
