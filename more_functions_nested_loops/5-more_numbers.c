8d3150aaa314aff741d2c60ddb90756e4df7c3fd
Mvke-IT <michael.duarte09@gmail.com>
2023-10-22 19:31:02 -0400

    push

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *more_numbers - print 10 times numbers from 0 to 14
+ */
+void more_numbers(void)
+{
+	int k = 1;
+	int a = 0;
+
+	while (k <= 10)
+	{
+		for (a = 0; a < 15; a++)
+		{
+
+			if (a > 9)
+			{
+				_putchar(a / 10 + '0');
+			}
+				_putchar(a % 10 + '0');
+		}
+		_putchar('\n');
+		k++;
+	}
+}
