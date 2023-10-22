1d53b786459a7adab2a1a09e03a505ff79bd6f0f
Mvke-IT <michael.duarte09@gmail.com>
2023-10-22 19:24:31 -0400

    push

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int digit = 0;
+
+	for (digit = 0; digit <= 9; digit++)
+		_putchar(digit % 10 + '0');
+	_putchar('\n');
+}
