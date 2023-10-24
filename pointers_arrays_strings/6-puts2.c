e19948bcb4ad463607a4a7a5ff6fbfd48d04561a
Mvke-IT <michael.duarte09@gmail.com>
2023-10-24 17:08:56 -0400

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,10 +7,11 @@
 void puts2(char *str)
 {
 	int i, m;
-	{
+	
 	i=_strlen(str);
 
 	for(m=o; m<i; m++)
+	{
 		if ((m%2)==0)
 		{
 			_putchar(str[m]);
@@ -20,4 +21,5 @@ void puts2(char *str)
 		continue;
 }
 }
+_putchar ('\n')
 }
