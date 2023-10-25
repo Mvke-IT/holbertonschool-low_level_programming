ff935577c46cc3ca6d1a5a3c6aed0a460204f530
Mvke-IT <michael.duarte09@gmail.com>
2023-10-25 09:14:42 -0400

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,15 @@
 #include "main.h"
-#include <string.h>
 /**
  * puts2 - Prints every other character
  *@str: is the string that's going to be modified.
  */
+
 void puts2(char *str)
 {
 	int i, m;
 
 	for (i = 0; str[i] != '\0'; i++)
-		;
+	;
 	for (m = 0; m < i; m++)
 	{
 		if ((m % 2)==0)
