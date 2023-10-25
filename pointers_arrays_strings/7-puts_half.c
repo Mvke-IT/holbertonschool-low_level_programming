5c01a7773d4ea5c11e1b78ffa4f78bd2c1bd9ea9
Mvke-IT <michael.duarte09@gmail.com>
2023-10-25 10:45:33 -0400

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h'
+/**
+ * puts_half-prints the second half of a string
+ *
+ * @str: string to print
+ */
+
+void puts_half(char *str)
+{
+	int i,j;
+
+	i = 0;
+	while (str [i] ! = '\0')
+		i++;
+	i--;
+
+	for (j=(i/2) = 1; j <= i; j++)
+		_putchar(str[j]);
+	_putchar ('\n')
+}
