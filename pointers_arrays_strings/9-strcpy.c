dad3dac189ba2b6b859ce7e456ecd798e5bd4ad5
Mvke-IT <michael.duarte09@gmail.com>
2023-10-25 14:58:26 -0400

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+include "main.h"
+/**
+ * *strcpy - runs a script that copy and paste the string
+ * @dest: where is going to paste the string
+ * @src: where is the string
+ * Return: Return the string
+ */
+
+char *_strcpy(char *dest, char *src);
+{
+	int 1 = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[1];
+		i++;
+	}
+	dest[i] = src[i]
+		return (dest);
+}
