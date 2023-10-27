a7d5a681f1719281d0c2dd8c8579b74638a84984
Mvke-IT <michael.duarte09@gmail.com>
2023-10-27 13:59:58 -0400

    1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 /**
  * _strncat - concatenates two strings
  *@dest: concatenated destination
@@ -7,22 +6,28 @@
  *@n: n is the number of bytes of src
  * Return: pointer to the resulting string
  */
-char *_strncat(char *dest, char *src, int n) {
-	size_t dest_len = 0;
-	size_t i;
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int len = _strlen(dest);
 
-	// Find the length of dest
-	for (i = 0; dest[i] != '\0'; i++) {
-		dest_len++;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[len + i] = src[i];
 	}
+	dest[len + i] = '\0';
+	return (dest);
+}
+/**
+ * _strlen - returns the length of a string
+ * @s: pinter to addres os strign
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int count = 0;
 
-	// Concatenate at most n bytes from src
-	for (i = 0; i < n && src[i] != '\0'; i++) {
-		dest[dest_len + i] = src[i];
-	}
-
-	// Add null-terminator to dest
-	dest[dest_len + i] = '\0';
-
-	return dest;
+	for (; s[count] != '\0'; count++)
+		;
+	return (count);
 }
