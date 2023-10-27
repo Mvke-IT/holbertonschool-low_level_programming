be836e4630b6bb3d32d0251255327debec0d5306
Mvke-IT <michael.duarte09@gmail.com>
2023-10-27 09:42:49 -0400

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * _strcat - concatenates two strings
  * @dest: pointer to the target srtring to be concatenated
@@ -7,26 +8,6 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int len = _strlen(dest);
-	int i;
-
-	for (i = 0; i < _strlen(src); i++)
-	{
-		dest[len + i] = src[i];
-	}
-	dest[len + i] = '\0';
-	return (dest);
-}
-/**
- * _strlen - returns the length of a string
- * @s: pinter to addres os strign
- * Return: string length
- */
-int _strlen(char *s)
-{
-	int count = 0;
-
-	for (; s[count] != '\0'; count++)
-		;
-	return (count);
+	strcpy (dest + strlen(dest), src);
+	return dest;
 }
