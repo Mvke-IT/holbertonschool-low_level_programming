76a774e927315cc68cbb4d38706aec55350487b0
Mvke-IT <michael.duarte09@gmail.com>
2023-10-30 11:43:28 -0400

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,37 +1,31 @@
-#include "main.h"
+#include"main.h"
 /**
- * _sqrt_recursion - return the natural square root of a number
- * @n: number
- * Return: 0
+ * find_square_root - find the square root
+ * @root: root number
+ * @n: random number
+ * Return: return square root if its perfecto otherwise -1.
  */
-int _sqrt_recursion(int n)
+int find_square_root(int root, int n)
 {
-	return (_sqrt_recursion2(1, n));
-}
-/**
- * _sqrt_recursion2 - find square root
- * @i: base
- * @n:number to find square root
- * Return: square root or -1
- **/
-int _sqrt_recursion2(int i, int n)
-{
-	if (n < 0)
+	int perfect_square = root * root;
+
+	if (perfect_square < n)
+	{
+		return (find_square_root(root + 1, n));
+	}
+	else if (perfect_square > n)
 	{
 		return (-1);
 	}
+	return (root);
+}
 
-	if (i == n)
-	{
-		return (1);
-	}
-
-	else if (i * i == n)
-	{
-		return (i);
-	}
-	else
-	{
-		return (_sqrt_recursion2(i + 1, n));
-	}
+/**
+ * _sqrt_recursion - return the natural square root of a number
+ * @n: random number to find square root
+ * Return: the square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (find_square_root(0, n));
 }
