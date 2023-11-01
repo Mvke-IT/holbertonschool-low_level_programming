7254de35705934bca250f4550792b084f70d3908
Mvke-IT <michael.duarte09@gmail.com>
2023-11-01 15:20:19 -0400

    2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - starts the program
+ * @argc: size of the string
+ * @argv: contains the programm command
+ * Return: Return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for(i = 0; 1 < argc; i++0)
+	{
+		printf("%s\n", argv [i]);
+	}
+	reutrn 0;
+}
