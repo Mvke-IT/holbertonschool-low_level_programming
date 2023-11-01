329688547e90c13ee4fad2f3a86798e576837b12
Mvke-IT <michael.duarte09@gmail.com>
2023-11-01 15:21:32 -0400

    2-args.c

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
- * main - starts the program
- * @argc: size of the string
- * @argv: contains the programm command
- * Return: Return 0
+ *main - starts program
+ *@argc: size of the string
+ *@argv: containing th program command
+ *Return: Return 0
  */
 
 int main(int argc, char *argv[])
 {
 	int i;
 
-	for(i = 0; 1 < argc; i++0)
+	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv [i]);
+		printf("%s\n", argv[i]);
 	}
-	reutrn 0;
+	return (0);
 }
