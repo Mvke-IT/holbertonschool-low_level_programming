271ce26185d338d5a731acae94ff82a9635bf01e
Mvke-IT <michael.duarte09@gmail.com>
2023-11-09 11:19:16 -0400

    wrote a function that creates an array of integers

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @num: amount of elementes
+ * @size: the data type
+ * Return: the value of the pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i, *n;
+	char *ch;
+
+	if (num == 0)
+	{
+		return (NULL);
+	}
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	if (size == sizeof(char))
+	{
+		ch = malloc(nmemb * size);
+		if (ch == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; i < num; i++)
+		{
+			ch[i] = '0';
+		}
+		return (ch);
+	}
+	else
+	{
+
+	n = malloc(nmemb * size);
+
+	if (n == NULL)
+	{
+		return (NULL);
+	}
+		for (i = 0; i < num; i++)
+		{
+			n[i] = 0;
+		}
+	}
+	return (n);
+}
