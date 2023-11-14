959a50298fde8f074fcfae67f98058a336fa8a33
Mvke-IT <michael.duarte09@gmail.com>
2023-11-14 10:16:19 -0400

    took out the else

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 #include "dog.h"
 /**
- * init_dog - write a function that initialize a
- * variable of type struct dog
+ * init_dog - write a function that initialize a variable of type struct dog
  * @d: struct pointer
  * @name: dog name
- * @age: dos age
+ * @age: dog age
  * @owner: dog owner
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 	{
-	}
-	else
-	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
 	}
-};
+}
