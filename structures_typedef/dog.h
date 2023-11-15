5f3bc1badf42c791716abc676b479ee4fc81b0e4
Mvke-IT <michael.duarte09@gmail.com>
2023-11-15 13:41:29 -0400

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,8 +14,7 @@ float age;
 char *owner;
 } dog_t;
 
-void print_dog(struct dog *d)
-void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
 #endif
