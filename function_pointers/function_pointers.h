22b2555f4ddf8ef0741e641463c4342f1debf610
Mvke-IT <michael.duarte09@gmail.com>
2023-11-16 16:37:55 -0400

    fixed pointer

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-#include <stdef.h>
+#include <stddef.h>
 #ifndef function_pointer_h
 #define function_pointer_h
 int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*m)(char *));
 #endif
