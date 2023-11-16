ea67ec2c65c6a1e3913fb5eecad2d1bd9c4e2aaa
Mvke-IT <michael.duarte09@gmail.com>
2023-11-16 16:17:34 -0400

    wrote a function that prints a name using function pointers

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * print_name - function that connects function that needs 1 char
+ * @m: is a pointer to function with a char
+ * @name: name to print
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (m != NULL && name != NULL)
+	{
+		m(name);
+	}
+}
