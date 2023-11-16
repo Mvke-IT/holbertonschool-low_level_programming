8928ed9a612b57c17343bfa8e52d16856cd316af
Mvke-IT <michael.duarte09@gmail.com>
2023-11-16 16:32:57 -0400

    fixed library

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stddef.h>
+#include <stdlib.h>
 /**
  * print_name - function that connects function that needs 1 char
  * @m: is a pointer to function with a char
