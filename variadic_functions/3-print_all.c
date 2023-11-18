d24fc838df7c37dec31011eaee84725d9f2a7963
Mvke-IT <michael.duarte09@gmail.com>
2023-11-18 17:49:20 -0400

    printing anything

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,44 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * print_all - print everything
+ * @format: type of arguments
+ */
+void print_all(const char * const format, ...)
+{
+	va_list object;
+	int i = 0;
+	char  *s;
+
+	va_start(object, format);
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+			case 'c':
+				printf("%c", va_arg(object, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(object, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(object, double));
+				break;
+			case 's':
+				s = va_arg(object, char *);
+				s ? printf("%s", s) : printf("(nil)");
+				break;
+			}
+			if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f'
+						|| format[i] == 's') && format[i + 1])
+			{
+				printf(", ");
+			}
+			i++;
+		}
+		va_end(object);
+	}
+		printf("\n");
+}
