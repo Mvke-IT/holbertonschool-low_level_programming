ceb253e7106156b655b6e97b7ff8a6cbd3ffcb1a
Mvke-IT <michael.duarte09@gmail.com>
2023-12-01 12:51:35 -0400

    adding end if

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,4 +1,9 @@
+#ifndef LIST_H
+#define LIST_H
 #include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -24,3 +29,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
+#endif
