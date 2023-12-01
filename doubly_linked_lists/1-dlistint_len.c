5517a06ba834a327e4a4f2d21d55ea2e2b34ef83
Mvke-IT <michael.duarte09@gmail.com>
2023-12-01 13:20:19 -0400

    fixed prototype functionx

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,19 +1,21 @@
 #include "lists.h"
-#include <stdio.h>
+
 /**
  * print_dlistint - function that prints all the elements of a list
  * @h: the pointer to the head node
  * Return: the number of nodes
  */
-size_t print_dlistint(const dlistint_t *h)
+
+size_t dlistint_len(const dlistint_t *h)
+
 {
 	int count = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
+		
 		h = h->next;
-		count += 1;
+		count++;
 	}
 	return (count);
 }
