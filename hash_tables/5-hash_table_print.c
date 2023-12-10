d5b17659254a38313c38497a9454ed7afd7ac7a3
Mvke-IT <michael.duarte09@gmail.com>
2023-12-10 17:18:06 -0400

    added prototype for function

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_print - function that prints a hash table
+ * @ht: is the hash table
+ *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i = 0, j = 0;
+	hash_node_t *node = NULL;
+
+	if (!ht)
+		return;
+	printf("{");
+	for (; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			if (j == 1)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			node = node->next;
+			j = 1;
+		}
+	}
+	printf("}\n");
+}
