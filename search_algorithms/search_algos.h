fc32e6f3915fd04436698fd71dbf03102ec225fe
Mvke-IT <michael.duarte09@gmail.com>
2024-01-19 10:08:58 -0400

    Wrote a function that searches for a value in a sorted array of integers using the Binary search algorithm

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef search_algos_h
+#define search_algos_h
+
+#include <stdio.h>
+
+int linear_search(int *arr, size_t size, int val);
+int binary_search(int *arr, size_t size, int val);
+
+#endif
